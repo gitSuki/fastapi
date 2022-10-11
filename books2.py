890ae3e26ea8d0f3680bcdc744c83d6217527644
gitSuki <collinbrown226@gmail.com>
2022-10-10 23:45:16 -0500

    Add books2 file and create Book class using pydantic

---
+++ books2.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from uuid import UUID
+
+app = FastAPI()
+
+class Book(BaseModel):
+    id: UUID
+    title: str
+    author: str
+    description: str
+    rating: int
+
+BOOKS = []
+
+@app.get('/')
+async def get_all_books():
+    return BOOKS
